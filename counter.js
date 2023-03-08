bc9d1101006ddd13dd417f38bd12a1ea96fe1b8a
Vivian Conrad <44071734+vivianconrad@users.noreply.github.com>
2023-03-08 17:21:38 -0600

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  }
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
